df6d160eedf812192cc733486b2ff98496948693
BernardooDev <bernardo.almeida@uscsonline.com.br>
2023-03-11 17:25:34 -0300

    Incluiding more files with python

---
+++ Booleans.py
@@ -0,0 +1,18 @@
+#Print a message based on whether the condition is True or False:
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#The bool() function allows you to evaluate any value, and give you True or False in return
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
---
+++ ReturnBoolean.py
@@ -0,0 +1,17 @@
+#Functions can Return a Boolean
+def myFunction() :
+  return True
+
+print(myFunction())
+
+#Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction() :
+  return False
+
+myFunction()
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
